60a78a00444a6bccffd4401af799bf1424549797
VSPO <hongyu@vspo.cn>
2023-08-18 17:30:47 +0800

    no message

---
+++ mall-tiny-mybatis-plus/src/main/java/com/macro/mall/tiny/service/impl/UmsPermissionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.macro.mall.tiny.service.impl;
+
+import com.macro.mall.tiny.pojo.UmsPermission;
+import com.macro.mall.tiny.mapper.UmsPermissionMapper;
+import com.macro.mall.tiny.service.IUmsPermissionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 后台用户权限表 服务实现类
+ * </p>
+ *
+ * @author yuri
+ * @since 2023-08-18
+ */
+@Service
+public class UmsPermissionServiceImpl extends ServiceImpl<UmsPermissionMapper, UmsPermission> implements IUmsPermissionService {
+
+}
